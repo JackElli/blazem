7fdca6dccdedcece8177cb931a07f0eef72e2933
Jack Ellis <jackellis100@hotmail.co.uk>
2023-05-14 14:15:27 +0100

    Now return 401 when unauth (as it should) and fixed the invalid key type issue

--- pkg/domain/endpoint/middleware.go
+++ pkg/domain/endpoint/middleware.go
@@ -13,33 +13,50 @@ func Middleware(f func(w http.ResponseWriter, req *http.Request)) func(w http.Re
 	return func(w http.ResponseWriter, req *http.Request) {
 		c, err := req.Cookie("token")
 		if err != nil {
-			w.WriteHeader(403)
+			w.WriteHeader(401)
 			return
 		}
 
 		jwtStr := c.Value
-		tkn, err := jwt.Parse(jwtStr, func(t *jwt.Token) (interface{}, error) {
-			return t, nil
-		})
+		jwtOk := parseJWT(jwtStr)
 
-		if tkn.Method != jwt.SigningMethodHS256 {
-			w.WriteHeader(403)
-			return
-		}
-
-		claims, ok := tkn.Claims.(jwt.MapClaims)
-		if !ok {
-			w.WriteHeader(403)
-			return
-		}
-
-		// check if token is in date
-		if int64(claims["exp"].(float64)) < time.Now().Local().Unix() {
-			w.WriteHeader(403)
+		if !jwtOk {
+			w.WriteHeader(401)
 			return
 		}
 
 		// continue with the passing of the methods
 		f(w, req)
 	}
+}
+
+// parseJWT checks if the JWT is valid and in date
+func parseJWT(jwtStr string) bool {
+	tkn, err := jwt.Parse(jwtStr, func(t *jwt.Token) (interface{}, error) {
+		return []byte("SecretYouShouldHide"), nil
+	})
+
+	if err != nil {
+		return false
+	}
+
+	if tkn.Method != jwt.SigningMethodHS256 {
+		return false
+	}
+
+	claims, ok := tkn.Claims.(jwt.MapClaims)
+	inDate := jwtInDate(claims)
+	if !ok || !inDate {
+		return false
+	}
+
+	return true
+}
+
+// jwtInDate checks whether the JWT (claims) section is in date
+func jwtInDate(claims jwt.MapClaims) bool {
+	if int64(claims["exp"].(float64)) < time.Now().Local().Unix() {
+		return false
+	}
+	return true
 }
